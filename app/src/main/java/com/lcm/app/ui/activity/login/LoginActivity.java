52ad7bd72f8fad82a2fdcbd29e7f6ba40d411b52
李晨鸣 <lichenming_hs@163.com>
2017-08-17 19:50:07 +0800

    完成登录页面排版

--- app/src/main/java/com/lcm/app/ui/activity/login/LoginActivity.java
+++ app/src/main/java/com/lcm/app/ui/activity/login/LoginActivity.java
@@ -1,28 +1,65 @@
 package com.lcm.app.ui.activity.login;
 
 
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Button;
+
 import com.lcm.android.base.BaseActivity;
+import com.lcm.app.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
-/**
- * A login screen that offers login via email/password.
- */
 public class LoginActivity extends BaseActivity {
 
 
+//    @BindView(R.id.toolbar)
+//    Toolbar toolbar;
+    @BindView(R.id.edt_email)
+    TextInputEditText edtEmail;
+    @BindView(R.id.til_email)
+    TextInputLayout tilEmail;
+    @BindView(R.id.edt_pwd)
+    TextInputEditText edtPwd;
+    @BindView(R.id.til_pwd)
+    TextInputLayout tilPwd;
+    @BindView(R.id.btn_login)
+    Button btnLogin;
+
     @Override
     protected int rootView() {
-        return 0;
+        return R.layout.activity_login;
     }
 
     @Override
     protected void initView() {
+//        toolbar.setTitle("登录");
+//        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void initData() {
 
     }
+
+
 }
 
